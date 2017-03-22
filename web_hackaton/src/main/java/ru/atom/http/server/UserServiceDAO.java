5535795e617577b8866788f5e36e8bfe05e662bb
Forwolk <nail.akhatov@gmail.com>
2017-03-22 19:00:59 +0300

    UserServiceDAO измнение методов + javaDoc

--- web_hackaton/src/main/java/ru/atom/http/server/UserServiceDAO.java
+++ web_hackaton/src/main/java/ru/atom/http/server/UserServiceDAO.java
@@ -4,7 +4,32 @@ import java.util.Set;
 
 public interface UserServiceDAO {
 
-    void login (String name);
-    void logout (String name);
+    /**
+     * Залогинить пользователя
+     *
+     * @param user кого залогинить
+     */
+    void login (User user);
+
+    /**
+     * Разлогинить пользователя
+     *
+     * @param user кого разлогинить
+     */
+    void logout (User user);
+
+    /**
+     * Получить пользователя по имени
+     *
+     * @param name имя пользователя
+     * @return объект User
+     */
+    User getUserByName (String name);
+
+    /**
+     * Получить список пользователей онлайн
+     *
+     * @return User'ы онлайн
+     */
     Set<User> getOnline ();
 }
